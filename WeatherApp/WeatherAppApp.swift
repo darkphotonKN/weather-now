b62835a1d2b89582976e1b63569ac1b8ba30b4cc
Kranti <darkphoton20@gmail.com>
2023-11-13 23:23:55 +0800

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Kranti on 2023/11/13.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
